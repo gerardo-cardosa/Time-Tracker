556bfca82d4587b9cd4ba8d651bb8ddaaaadf52f
Gerardo Gonzalez <gerardo.cardosa@gmail.com>
2016-01-08 11:30:07 -0800

    New Update Jan 08

--- js/tracker.js
+++ js/tracker.js
@@ -8,6 +8,7 @@ var START_TASK = 'start';
 
 
 
+
 var googleAuth = new OAuth2('google', {
   client_id: '419716539860-dfj8h05gackg138jnhls0u4q5l1u5e5q.apps.googleusercontent.com',
   client_secret: 'rqra1MNGHREYt2zYBxRBfvGS',
@@ -28,6 +29,11 @@ $(document).ready(function(){
 		buttonAuthorize();
 	});
 });
+
+$(window).unload(function(){
+	$('.titlename_dps').text('closeeeeeeeeeeeeeeeeeeeeeeeeee');
+});
+
 
 function showContent(){
 	if(googleAuth.hasAccessToken())
@@ -63,6 +69,59 @@ function modifyContent(resp, xhr)
 	//var content = $('#content');
 	//var string = JSON.stringify(resp.items[0]);
 	//content.html(resp);
+}
+
+function getItem(id)
+{
+	for(var i = 0 ; i< info.length; i++)
+		{
+			if( info[i].Task_ID != undefined &&  info[i].Task_ID ==  id)
+			{
+				return  info[i];
+			}
+		}
+	return undefined;
+}
+
+function isRepeated(item)
+{
+	var value = false;
+	console.log('is repeated ' + item.val() );
+	var par =  item.parent()
+	par.siblings().each(function(){ 
+		console.log($(this).children().first().val());
+		if($(this).children('input').first().val() == item.val())
+		{
+			value = true;
+			console.log('value: ' + value );
+		}
+	});
+	return value;
+}
+
+function setID(item)
+{
+	console.log("setId function " + item);
+	var item = $('#'+item);
+	var sibling = item.siblings('a');
+	if(item.val()!='' && getItem(item.val()) == undefined && !isRepeated(item))
+	{	
+		var parent = item.closest('p');
+		parent.attr('id', item.val());		
+		sibling.css('display','block');	
+		sibling.unbind('click');	
+		sibling.click(item.val(), function (e) {			
+			e.preventDefault();
+			console.log("e.data " + e.data);	
+			taskInterface.toggleTimer(e.data);
+		});
+
+	}
+	else
+	{
+		sibling.css('display','none');
+	}
+	
 }
 
 
